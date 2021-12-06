7e20a9323da7b83aecdb2002a81f71a93a1f0d00
Hedi1312 <hediokba18@gmail.com>
2021-12-06 21:42:59 +0100

    "debut page connexion"

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+
+require_once "./Connexion.php";
+session_start();
+
+if (!isset($_GET['module'])) {
+    $module="ModConnexion";
+}
+else {
+    $module = $_GET['module'];
+}
+
+Connexion::initConnexion();
+
+switch ($module) {
+    case "ModJoueurs":
+    case "ModEquipe":
+    case "ModConnexion":
+    case "ModProfil":
+    case "ModAccueil":
+    case "ModInscription":
+        require_once "./modules/$module/$module.php";
+        require_once "./modules/Affichage/header.php";
+        new $module();
+        break;
+    default :
+        die("Interdiction d'accès");
+}
+require_once "./modules/Affichage/footer.php";
+
+?>
--- modules/Affichage/header.php
+++ modules/Affichage/header.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./modules/Affichage/Css/Style.css" type="text/css" />
+    <title><?= $title?></title>
+</head>
+<body>
+<div class="topnav">
+
+    <a href="index.php?module=ModAccueil" id="titre">BasicFoot</a>
+    <a href="index.php?module=ModProfil" ">Mon compte</a>
+
+</div>
+</body>
+</html>
