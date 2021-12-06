8edd06686cbcb774eda57b22d2596b443c20b1ac
Hedi1312 <hediokba18@gmail.com>
2021-12-06 17:34:27 +0100

    "debut page connexion"

---
+++ Connexion.php
@@ -0,0 +1,24 @@
+<?php
+class Connexion {
+
+    protected static $bdd=NULL;
+
+    public function __construct () {
+    }
+
+    public static function initConnexion() {
+
+        //test de la connexion
+        try {
+            $dns="mysql:host=localhost;dbname=basicfoot";
+            $user="root";
+            self::$bdd = new PDO($dns,$user);
+            self::$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+}
+?>
