b77f11a51add1e28a8c34d8a2891fcddfb84cb0b
omar SALEK <omarsalek05@gmail.com>
2021-12-10 01:22:51 +0100

    "Profil V1"

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once './Connexion.php';
+if (!isset($_GET['module'])) {
+        $_GET['module']="ModProfil";
+}
+$module = $_GET['module'];
+
+switch ($module) {
+    case "ModProfil":
+        require_once "./modules/$module/$module.php";
+        new $module();
+        break;
+    default :
+        die("Interdiction d'accès");
+}
+Connexion::initConnexion();
+
+?>
