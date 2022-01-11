ac45fe5daded3d5efe306f99358dda77f74d147f
omar SALEK <omarsalek05@gmail.com>
2022-01-11 13:52:11 +0100

    "Affichage des messages entre les participants "

--- Vue/Affichage/discussionMatchs.php
+++ Vue/Affichage/discussionMatchs.php
@@ -40,38 +40,40 @@
 
 </div>-->
 
+
 <div class="container-fluid h-100">
     <div class="row justify-content-center h-100">
         <div class="col-md-8 col-xl-6 chat">
             <div class="cardDiscussion">
                 <div class="card-header msg_head"></div>
+
+                <div id="msgForm" >
                 <div class="card-body msg_card_body">
-
+                    <?php
+                    $liste = $data["messages"];
+                    if (!empty($liste)): ?>
+                    <?php foreach ($liste as $value): ?>
+                   <?php if ($value['idUtilisateur'] == $userId[0][0]): ?>
                     <div class="d-flex justify-content-end mb-4">
                         <div class="msg_cotainer_send">
-                            zdefzfjhzjfzefjf
-                            <span class="msg_time_send">8:55 AM, Today</span>
+                            <span id="<?= $value['idUtilisateur'] ?>"><?= nl2br($value['contenu']) ?></span>
+                            <span class="msg_time_send"><?= $value['DatePublication'] ?></span>
                         </div>
                     </div>
-
+                     <?php else: ?>
                     <div class="d-flex justify-content-start mb-4">
-
                         <div class="msg_cotainer">
-                            I am good too, thank you for your chat template
-                            <span class="msg_time">9:00 AM, Today</span>
+                            <span id="<?= $value['idUtilisateur'] ?>"><?= nl2br($value['contenu']) ?></span>
+                            <span class="msg_time"><?= $value['DatePublication'] ?></span>
                         </div>
                     </div>
-
-                    <div class="d-flex justify-content-end mb-4">
-                        <div class="msg_cotainer_send">
-                            You are welcome
-                            <span class="msg_time_send">9:05 AM, Today</span>
-                        </div>
-                    </div>
-
-
+                    <?php endif; ?>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                    <script>document.getElementById('msgForm').scrollTop = document.getElementById('msgForm').scrollHeight;
+                    </script>
                 </div>
-
+                </div>
                 <div class="card-footer">
                     <form name="message" action="index.php?module=ModDiscussion&action=EnvoyerMessage&id=<?= $data[0]?>" method="post">
                         <div class="input-group">
