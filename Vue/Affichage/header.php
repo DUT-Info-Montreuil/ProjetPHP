0c3d09c8124e3af6c7582d43405e7f240355069f
hokba <hediokba18@gmail.com>
2022-01-12 12:35:46 +0100

    maj

--- Vue/Affichage/header.php
+++ Vue/Affichage/header.php
@@ -11,7 +11,7 @@
 
 </head>
 <body>
-
+<div id="wrapper2">
 <header >
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #130255;" >
         <div class="container-fluid">
@@ -51,6 +51,6 @@
     </nav>
 </header>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 
 
