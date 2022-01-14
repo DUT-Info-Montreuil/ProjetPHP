4f72b3676653e3339f1eaca1df3f6756c4b41253
omar SALEK <62855151+omarsalek@users.noreply.github.com>
2022-01-14 16:32:37 +0100

    Update photosMatchs.php

--- Vue/Affichage/photosMatchs.php
+++ Vue/Affichage/photosMatchs.php
@@ -26,42 +26,48 @@
 $liste = $data["liste"];
 $liste2 = $data["liste2"];
 
-if (!empty($liste) || !empty($liste2)): ?>
+if ((!empty($liste) || !empty($liste2)) ): ?>
 
 
 <section class="gallery min-vh-100">
 
         <div class="container-lg">
-
         <div class="row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-4">
             <?php foreach ( $liste as $elements) : ?>
+                <?php if ($elements['photo']!=NULL) :?>
                 <div class="col">
                 <img  id="photoMatchsConsultation" src="./Vue/Affichage/Images/<?= $elements['photo'] ?>" class="gallery-item" alt="gallery">
                 </div>
+                <?php endif; ?>
             <?php endforeach; ?>
+
             <?php foreach ( $liste2 as $elements) : ?>
+                <?php if ($elements['photo']!=NULL) :?>
+
                 <div class="col">
                     <img  id="photoMatchsConsultation" src="./Vue/Affichage/Images/<?= $elements['photo'] ?>" class="gallery-item" alt="gallery">
                 </div>
+                <?php endif; ?>
             <?php endforeach; ?>
         </div>
         </div>
 </section>
 
-<!-- Modal -->
 <div class="modal fade" id="gallery-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+          
             <div class="modal-body">
+                
                 <img  id="photoMatchsConsultation" src="./Vue/Affichage/Images/<?= $elements['photo'] ?>" class="modal-img" alt="modal img">
             </div>
         </div>
     </div>
 </div>
+
 
 <?php else: ?>
 <div class="alert alert-danger mt-5">il ya aucun photos !</div>
