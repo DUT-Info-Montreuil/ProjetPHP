c0eda77c5ca8867e3f480b78009a7d709a9cf20e
Hedi1312 <hediokba18@gmail.com>
2022-01-17 14:04:05 +0100

    merge notation et le dev

--- Connexion.php
+++ Connexion.php
@@ -8,13 +8,13 @@ class Connexion {
 
     public static function initConnexion() {
         try {
-            $dns="mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201646";
+           /* $dns="mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201646";
             $user="dutinfopw201646";
             $password="bygyjyjy";
-            self::$bdd = new PDO($dns,$user,$password);
+            self::$bdd = new PDO($dns,$user,$password);*/
 
-            //$dns="mysql:host=localhost;dbname=basicfoot";
-            //$user="root";
+            $dns="mysql:host=localhost;dbname=basicfoot";
+            $user="root";
 
             self::$bdd = new PDO($dns,$user);
             self::$bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
